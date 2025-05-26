8597d67dde01513a1f96a300326c6e8e4ccefb03
Oleksandr Kozlov <wpok13@gmail.com>
2025-05-26 23:26:16 +0200

    3_methods tasks completed

---
+++ ruby/3_methods/6_arguments.rb
@@ -0,0 +1,1 @@
+# should be two arguments. given one.
---
+++ ruby/3_methods/say.rb
@@ -0,0 +1,11 @@
+def say(words='hello')
+  puts "**********"
+  puts words + "."
+  puts "**********"
+  puts ""
+end
+
+say()
+say("hi")
+say("how are you")
+say("I'm fine")
