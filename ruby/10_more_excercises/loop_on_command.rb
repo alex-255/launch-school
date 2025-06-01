6669beb86d31a6c13b4fe82c80f1d5712e97261f
Oleksandr Kozlov <wpok13@gmail.com>
2025-06-01 18:07:42 +0200

    say hello

--- ruby/10_more_excercises/loop_on_command.rb
+++ ruby/10_more_excercises/loop_on_command.rb
@@ -1,7 +1,6 @@
 loop do
   puts 'Should I stop looping?'
   answer = gets.chomp
-  if answer.downcase == 'yes'
-    break
-  end
+  break if answer.downcase == 'yes'
+  puts "That's not the right answer. Please answer 'yes' to stop."
 end
