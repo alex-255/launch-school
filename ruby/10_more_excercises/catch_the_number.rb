89f552b2c6557c9e272fa8e41a0c0c22d4bf7c23
Oleksandr Kozlov <wpok13@gmail.com>
2025-06-01 21:20:43 +0200

    catch the number

---
+++ ruby/10_more_excercises/catch_the_number.rb
@@ -0,0 +1,8 @@
+loop do
+  number = rand(100)
+  if number >= 0 && number <= 10
+    puts "The number is #{number}, which is between 0 and 10. Exiting the loop."
+    break
+  end
+  puts number
+end
