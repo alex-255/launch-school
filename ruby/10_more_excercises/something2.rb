c1fa5e388c3f0c0a037cf6342cb0e004d3f21a96
Oleksandr Kozlov <wpok13@gmail.com>
2025-06-02 20:20:41 +0200

    something 2

--- ruby/10_more_excercises/something2.rb
+++ ruby/10_more_excercises/something2.rb
@@ -2,13 +2,12 @@
 loop do
   puts ">> Do you want me to print something? (y/n)"
   answer = gets.chomp.downcase
-  if answer == 'y'
-    puts "something"
-    break
-  elsif answer == 'n'
+  if %w(y n).include?(answer)
+    if answer == 'y'
+      puts "something"
+    end
     break
   else
-    puts "Invalid input! Please enter 'y' or 'n'."
-    next
+    puts "Invalid input! Please enter y or n."
   end
 end
