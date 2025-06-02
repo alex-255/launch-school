d4e732d86a31e410cfb8557b0eafbb53b281457a
Oleksandr Kozlov <wpok13@gmail.com>
2025-06-02 20:35:38 +0200

    password

---
+++ ruby/10_more_excercises/password.rb
@@ -0,0 +1,13 @@
+# password.rb
+PASSWORD = "SecreT"
+
+loop do
+puts ">> Please enter your password:"
+input = gets.chomp
+  if input == PASSWORD
+    puts "Welcome!"
+    break
+  else
+    puts ">> Invalid password!"
+  end
+end
