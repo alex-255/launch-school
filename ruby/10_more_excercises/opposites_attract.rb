6fc22f82b0322d84ab90c036876855f441527756
Oleksandr Kozlov <wpok13@gmail.com>
2025-06-03 23:21:10 +0200

    opposites attract

---
+++ ruby/10_more_excercises/opposites_attract.rb
@@ -0,0 +1,26 @@
+def valid_number?(number_string)
+  number_string.to_i.to_s == number_string && number_string.to_i != 0
+end
+
+number1 = nil
+number2 = nil
+
+loop do
+  puts ">> Please enter the first number (non-zero integer):"
+  string1 = gets.chomp
+  puts ">> Please enter the second number (non-zero integer):"
+  string2 = gets.chomp
+  if valid_number?(string1) && valid_number?(string2)
+    number1 = string1.to_i
+    number2 = string2.to_i
+    if number1 < 0 && number2 > 0 || number1 > 0 && number2 < 0
+      break
+    else
+      puts ">> Sorry. One integer must be positive, one must be negative."
+    end
+  else
+    puts ">> Invalid input! Only non-zero integers are allowed."
+  end
+end
+
+puts ">> #{number1} + #{number2} = #{number1 + number2}"
