ecd04cf0f4c840009ba3bdfb06a4408748c09190
Oleksandr Kozlov <wpok13@gmail.com>
2025-06-05 10:37:32 +0200

    Add alternative method for adding a dog to the pets hash

--- ruby/10_more_excercises/debugging/pets.rb
+++ ruby/10_more_excercises/debugging/pets.rb
@@ -1,5 +1,7 @@
 pets = { cat: 'fluffy', dog: ['sparky', 'fido'], fish: 'oscar' }
 
 pets[:dog].push('bowser')
+# or
+# pets[:dog] << 'bowser'
 
 p pets # => {:cat=>"fluffy", :dog=>["sparky", "fido", "bowser"], :fish=>"oscar"}
