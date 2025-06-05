46cee1f7cb4f43ccd1228c60a23342f163805b4b
Oleksandr Kozlov <wpok13@gmail.com>
2025-06-05 10:42:59 +0200

    Add comments to clarify the selection of even numbers in even_numbers.rb

--- ruby/10_more_excercises/debugging/even_numbers.rb
+++ ruby/10_more_excercises/debugging/even_numbers.rb
@@ -2,6 +2,8 @@ numbers = [5, 2, 9, 6, 3, 1, 8]
 
 even_numbers = numbers.select do |n|
   n if n.even?
+  # or
+  # n.even? - just n.even? returns true or false
 end
 
 p even_numbers # expected output: [2, 6, 8]
