2c92ba94c4b49e9696f09d320d5f1278c7a8f31d
Oleksandr Kozlov <wpok13@gmail.com>
2025-06-05 10:30:32 +0200

    Add weather_forecast.rb to predict and display weather conditions

---
+++ ruby/10_more_excercises/debugging/weather_forecast.rb
@@ -0,0 +1,12 @@
+def predict_weather
+  sunshine = [true, false].sample
+
+  if sunshine
+    puts "Today's weather will be sunny!"
+  else
+    puts "Today's weather will be cloudy!"
+  end
+end
+
+# Call the method to see the output
+predict_weather
