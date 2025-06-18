427942ed712bbd0dd3e6460f32e4bc3cabdf7fe9
Oleksandr Kozlov <wpok13@gmail.com>
2025-06-18 19:35:35 +0200

    Add concatenation_nums.py and concatenation_nums_coersion.py for string concatenation examples

---
+++ python/basic_operations/concatenation_nums.py
@@ -0,0 +1,2 @@
+print('10' + '20' + '30')  # Concatenation of strings
+print(10 + 20 + 30)        # Addition of integers
---
+++ python/basic_operations/concatenation_nums_coersion.py
@@ -0,0 +1,2 @@
+print(int('10') + int('20'))  # Concatenation of strings
+print(10 + 20)        # Addition of integers
