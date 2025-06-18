d4fa714c5c1d3146b93bc0f16c2ace6601c2733d
Oleksandr Kozlov <wpok13@gmail.com>
2025-06-18 23:23:20 +0200

    Add greeter.py for greeting messages and names.py for variable naming conventions

---
+++ python/variables/greeter.py
@@ -0,0 +1,8 @@
+# greeter.py
+
+def greet(name):
+    print(f"Good Morning, {name}.")
+    print(f"Good Afternoon, {name}.")
+    print(f"Good Evening, {name}.")
+
+greet("Victor")
---
+++ python/variables/names.py
@@ -0,0 +1,8 @@
+# index # idiomatic variable names
+# CatName # non-idiomatic variable names
+# lazy_dog # idiomatic variable names
+# quick_Fox # non-idiomatic variable names
+# 1stCharacter # illegal variable name in Python
+# operand2 # idiomatic variable names
+# BIG_NUMBER # idiomatic variable names
+# π # illegal variable name in Python
