a9e223df1a53548379de680df3953e2a254c6598
Oleksandr Kozlov <wpok13@gmail.com>
2025-08-11 23:47:58 +0200

    Add true or false expressions and conditional number check scripts

---
+++ ruby-2/4_flow_control/1.true_or_false.rb
@@ -0,0 +1,6 @@
+# result of the following expressions in comments:
+(32 * 4) >= 129 # false
+false != !true # false
+true == 4 # false
+false == (847 == '847') # true
+(!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false # true
