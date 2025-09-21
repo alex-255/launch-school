3702afe2d737724aa61eb9a9aff36de4707fa1bf
Oleksandr Kozlov <wpok13@gmail.com>
2025-09-22 01:22:54 +0200

    Add movies array with titles and release years

---
+++ ruby-3/1_basics/4.movies_array.rb
@@ -0,0 +1,15 @@
+movies = [
+  { title: "The Godfather", year: 1972 },
+  { title: "The Shawshank Redemption", year: 1994 },
+  { title: "Schindler's List", year: 1993 },
+  { title: "Raging Bull", year: 1980 },
+  { title: "Casablanca", year: 1942 },
+  { title: "Citizen Kane", year: 1941 }
+]
+
+puts movies[0][:year]
+puts movies[1][:year]
+puts movies[2][:year]
+puts movies[3][:year]
+puts movies[4][:year]
+puts movies[5][:year]
