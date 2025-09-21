3fd86eba9113a6cb153b8c7180b92cd5e5ddddc9
Oleksandr Kozlov <wpok13@gmail.com>
2025-09-22 01:19:52 +0200

    Add movies hash and print statements for classic films

---
+++ ruby-3/1_basics/3.movies.rb
@@ -0,0 +1,15 @@
+movies = {
+  "The Godfather" => 1972,
+  "The Shawshank Redemption" => 1994,
+  "Schindler's List" => 1993,
+  "Raging Bull" => 1980,
+  "Casablanca" => 1942,
+  "Citizen Kane" => 1941
+}
+
+puts movies["The Godfather"]
+puts movies["Casablanca"]
+puts movies["Citizen Kane"]
+puts movies["Raging Bull"]
+puts movies["The Shawshank Redemption"]
+puts movies["Schindler's List"]
