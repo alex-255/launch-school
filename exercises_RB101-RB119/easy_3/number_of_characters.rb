abf73abaeb120aff6b80027bd35c183cc596b2de
Oleksandr Kozlov <wpok13@gmail.com>
2025-10-01 22:34:27 +0200

    remove all whitespaces characters

--- exercises_RB101-RB119/easy_3/number_of_characters.rb
+++ exercises_RB101-RB119/easy_3/number_of_characters.rb
@@ -1,4 +1,4 @@
 puts "Please write word or multiple words:"
 input = gets.chomp
-number_of_characters = input.delete(" ").size
+number_of_characters = input.gsub(/\s/,'').size
 puts "There are #{number_of_characters} characters in \"#{input}\"."
