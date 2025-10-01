1e17e009df6219fd00952917cde1b8a7e02e367a
Oleksandr Kozlov <wpok13@gmail.com>
2025-10-01 22:50:36 +0200

    add test cases

--- exercises_RB101-RB119/easy_3/xor.rb
+++ exercises_RB101-RB119/easy_3/xor.rb
@@ -12,3 +12,7 @@ puts xor?(5.even?, 4.even?) == true
 puts xor?(5.odd?, 4.odd?) == true
 puts xor?(5.odd?, 4.even?) == false
 puts xor?(5.even?, 4.odd?) == false
+puts xor?(true, false) == true
+puts xor?(false, true) == true
+puts xor?(true, true) == false
+puts xor?(false, nil) == false
