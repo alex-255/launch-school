5e31f67aa3e3d9ee652c90fcb223bc265fc9145b
Oleksandr Kozlov <wpok13@gmail.com>
2025-10-02 15:22:03 +0200

    typo

--- exercises_RB101-RB119/easy_4/leap_year_2.rb
+++ exercises_RB101-RB119/easy_4/leap_year_2.rb
@@ -14,7 +14,7 @@ end
 
 # puts leap_year?(2016) == true
 # puts leap_year?(2015) == false
-# puts leap_year?(2100) == falsa
+# puts leap_year?(2100) == false
 # puts leap_year?(2400) == true
 # puts leap_year?(240000) == true
 # puts leap_year?(240001) == false
