0572da6bddefbf987e3c3d57cdec0674ccc3381a
Oleksandr Kozlov <wpok13@gmail.com>
2025-10-03 20:55:45 +0200

    new version of method multisum

--- exercises_RB101-RB119/easy_4/3_and_5.rb
+++ exercises_RB101-RB119/easy_4/3_and_5.rb
@@ -1,13 +1,20 @@
-def multiply_3_and_5(number)
-  (1..number).select do |n| 
-    n % 3 == 0 || n % 5 == 0 
-  end.sum
+def multisum(number)
+  (1..number).reduce(0) do |sum, n| 
+    (n % 3 == 0 || n % 5 == 0) ? sum + n : sum 
+  end
 end
 
-puts multiply_3_and_5(10) == 33
-puts multiply_3_and_5(1000) == 234168
-puts multiply_3_and_5(1) == 0
-puts multiply_3_and_5(3) == 3
-puts multiply_3_and_5(5) == 8
-puts multiply_3_and_5(100) == 2418
+# previous solution
+# def multisum(number)
+#   (1..number).select do |n|
+#     n % 3 == 0 || n % 5 == 0
+#   end.sum
+# end
+
+puts multisum(10) == 33
+puts multisum(1000) == 234168
+puts multisum(1) == 0
+puts multisum(3) == 3
+puts multisum(5) == 8
+puts multisum(100) == 2418
 
