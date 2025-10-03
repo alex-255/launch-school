3724d676f876c095fa25a344c3682e43dbf37f56
Oleksandr Kozlov <wpok13@gmail.com>
2025-10-03 21:49:18 +0200

    integer to string method

---
+++ exercises_RB101-RB119/easy_4/integer_to_string.rb
@@ -0,0 +1,14 @@
+def integer_to_string(number)
+  digits = number.digits
+  str_digits = digits.reverse.map { |digit| (digit + "0".ord).chr }
+  str_digits.join
+end
+  
+puts integer_to_string(4321) == '4321'
+puts integer_to_string(0) == '0'
+puts integer_to_string(5000) == '5000'
+puts integer_to_string(1234567890) == '1234567890'
+puts integer_to_string(100200300) == '100200300'
+puts integer_to_string(12345678901234567890) == '12345678901234567890'
+puts integer_to_string(000123) == '123'
+puts integer_to_string(100200300) == '100200300'
