28684b67f95409512874db9ae17829092d55f2fe
Oleksandr Kozlov <wpok13@gmail.com>
2025-10-07 20:38:56 +0200

    bug fix - two methods

--- exercises_RB101-RB119/easy_5/midnight2.rb
+++ exercises_RB101-RB119/easy_5/midnight2.rb
@@ -1,16 +1,18 @@
-def minutes(time)
+def minutes_before_midnight(time)
   hours_and_minutes = time.split(':')
   hours = hours_and_minutes[0].to_i
   minutes = hours_and_minutes[1].to_i
-
-  if hours <= 12 
-    hours * 60 + minutes
-  else
-    (hours * 60 + minutes) - 1440
-  end
+  (hours * 60 + minutes) - 1440
 end
 
-p minutes("10:10")
-p minutes("1:15")
-p minutes("11:57")
-p minutes("23:57")
+def minutes_after_midnight(time)
+  hours_and_minutes = time.split(':')
+  hours = hours_and_minutes[0].to_i
+  minutes = hours_and_minutes[1].to_i
+  hours * 60 + minutes
+end
+
+p minutes_after_midnight("10:10")
+p minutes_after_midnight("1:15")
+p minutes_after_midnight("11:57")
+p minutes_before_midnight("23:57")
