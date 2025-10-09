88f7324507f98d6754184a4b5c15574e0ba87777
Oleksandr Kozlov <wpok13@gmail.com>
2025-10-10 00:02:23 +0200

    add method for counting word sizes for only letters

---
+++ exercises_RB101-RB119/easy_5/word_sizes2.rb
@@ -0,0 +1,15 @@
+def word_sizes(string)
+  sizes = Hash.new(0)
+  words = string.split
+  words.each do |word|
+    clean_word = word.delete('^A-Za-z')
+    next if clean_word.empty?
+    sizes[clean_word.size] += 1
+  end
+  sizes
+end
+
+p word_sizes('Four score and seven.') 
+p word_sizes('Hey diddle diddle, the cat and the fiddle!')
+p word_sizes("What's up doc? ???")
+p word_sizes('')
