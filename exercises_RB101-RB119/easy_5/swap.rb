76b308f9cf536685ff674ecf27a917cf04b5b66d
Oleksandr Kozlov <wpok13@gmail.com>
2025-10-09 21:03:35 +0200

    add method for swapping letters in words

---
+++ exercises_RB101-RB119/easy_5/swap.rb
@@ -0,0 +1,21 @@
+def swap_words(string)
+  words = string.split
+  new_words = words.map do |word|
+    if word.length > 1
+      first_letter = word[0]
+      last_letter = word[-1]
+      middle = word[1..-2]
+      last_letter + middle + first_letter
+    else
+      word
+    end
+  end
+  new_words.join(' ')
+end
+
+p swap_words('Oh what a wonderful day it is')
+p swap_words('Abcde')
+p swap_words('a')
+p swap_words('a A')
+p swap_words('')
+p swap_words('a bc def ghij')
