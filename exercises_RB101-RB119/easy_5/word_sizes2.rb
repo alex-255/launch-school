98e02c16cf5ff950608c67b593d0427de5e9a05b
Oleksandr Kozlov <wpok13@gmail.com>
2025-10-10 00:05:25 +0200

    fix according to test cases

--- exercises_RB101-RB119/easy_5/word_sizes2.rb
+++ exercises_RB101-RB119/easy_5/word_sizes2.rb
@@ -11,5 +11,5 @@ end
 
 p word_sizes('Four score and seven.') 
 p word_sizes('Hey diddle diddle, the cat and the fiddle!')
-p word_sizes("What's up doc? ???")
+p word_sizes("What's up doc?")
 p word_sizes('')
