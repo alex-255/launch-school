d9fd1c8c063fd357be34b4cffe39c999f31b6427
Oleksandr Kozlov <wpok13@gmail.com>
2025-10-09 20:55:39 +0200

    add constant

--- exercises_RB101-RB119/easy_5/midnight2.rb
+++ exercises_RB101-RB119/easy_5/midnight2.rb
@@ -4,7 +4,7 @@ def minutes_before_midnight(time)
   hours_and_minutes = time.split(':')
   hours = hours_and_minutes[0].to_i
   minutes = hours_and_minutes[1].to_i
-  (1440 - (hours * 60 + minutes)) % MINUTES_PER_DAY
+  (MINUTES_PER_DAY - (hours * 60 + minutes)) % MINUTES_PER_DAY
 end
 
 def minutes_after_midnight(time)
