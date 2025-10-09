df7409b756e23aee0cdf0a813bb5e37154db9f3b
Oleksandr Kozlov <wpok13@gmail.com>
2025-10-09 21:10:12 +0200

    add a helper method for swapping letters in word

--- exercises_RB101-RB119/easy_5/swap.rb
+++ exercises_RB101-RB119/easy_5/swap.rb
@@ -1,14 +1,18 @@
+def swap_letters(word)
+  if word.length > 1
+    first_letter = word[0]
+    last_letter = word[-1]
+    middle = word[1..-2]
+    last_letter + middle + first_letter
+  else
+    word
+  end
+end
+
 def swap_words(string)
   words = string.split
   new_words = words.map do |word|
-    if word.length > 1
-      first_letter = word[0]
-      last_letter = word[-1]
-      middle = word[1..-2]
-      last_letter + middle + first_letter
-    else
-      word
-    end
+    swap_letters(word)
   end
   new_words.join(' ')
 end
@@ -17,5 +21,5 @@ p swap_words('Oh what a wonderful day it is')
 p swap_words('Abcde')
 p swap_words('a')
 p swap_words('a A')
-p swap_words('')
+p swap_words(' ').length
 p swap_words('a bc def ghij')
