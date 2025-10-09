7c8dc6ec96ef510d1eb402e949855b9055430402
Oleksandr Kozlov <wpok13@gmail.com>
2025-10-09 20:51:56 +0200

    fix bug

--- exercises_RB101-RB119/easy_5/midnight2.rb
+++ exercises_RB101-RB119/easy_5/midnight2.rb
@@ -2,7 +2,7 @@ def minutes_before_midnight(time)
   hours_and_minutes = time.split(':')
   hours = hours_and_minutes[0].to_i
   minutes = hours_and_minutes[1].to_i
-  1440 - (hours * 60 + minutes)
+  (1440 - (hours * 60 + minutes)) % 1440
 end
 
 def minutes_after_midnight(time)
@@ -17,3 +17,4 @@ p minutes_after_midnight("1:15")
 p minutes_after_midnight("11:57")
 p minutes_before_midnight("23:57")
 p minutes_before_midnight("24:00")
+p minutes_before_midnight("00:00")
