93ffb8fb213336e12b226805d1ac0446ee5b4314
Oleksandr Kozlov <wpok13@gmail.com>
2025-10-10 23:03:43 +0200

    add squeeze method for every character

---
+++ exercises_RB101-RB119/easy_5/crunch.rb
@@ -0,0 +1,23 @@
+def crunch(string)
+  array = string.split("")
+  new_array = []
+  array.each_with_index do |char, index|
+    if index > 0
+      if array[index - 1] != char
+        new_array.push(array[index - 1])
+      end
+      if index == array.length - 1
+        new_array.push(char)
+      end
+    elsif index == 0 && array.length == 1
+      new_array.push(char)
+    end
+  end
+  new_array
+end
+
+p crunch('ddaaiillyy ddoouubbllee') #== 'daily double'
+p crunch('4444abcabccba')           #== '4abcabcba'
+p crunch('ggggggggggggggg')         #== 'g'
+p crunch('a')                       #== 'a'
+p crunch('')                        #== ''
