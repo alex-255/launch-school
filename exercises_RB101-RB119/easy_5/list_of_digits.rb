211b2792844e276265b12b1e10a0eaf2b738ee03
Oleksandr Kozlov <wpok13@gmail.com>
2025-10-10 23:20:35 +0200

    convert integer to the list of digits

---
+++ exercises_RB101-RB119/easy_5/list_of_digits.rb
@@ -0,0 +1,11 @@
+def list_of_digits(number)
+  array = number.to_s.split('')
+  array.map { |digit| digit.to_i }
+end
+
+p list_of_digits(2453)
+
+puts list_of_digits(12345) == [1, 2, 3, 4, 5]     # => true
+puts list_of_digits(7) == [7]                     # => true
+puts list_of_digits(375290) == [3, 7, 5, 2, 9, 0] # => true
+puts list_of_digits(444) == [4, 4, 4]             # => true
