2e93bfa01e4b263d501c5049cf7b092a77589db5
Oleksandr Kozlov <wpok13@gmail.com>
2025-10-12 21:33:16 +0200

    add a method for removing vowels

---
+++ exercises_RB101-RB119/easy_6/remove_vowels.rb
@@ -0,0 +1,8 @@
+def remove_vowels(array)
+  array.map { |word| word.delete('aeiouAEIOU') }
+end
+
+p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz))
+p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+p remove_vowels(%w(green YELLOW black white))
+p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
