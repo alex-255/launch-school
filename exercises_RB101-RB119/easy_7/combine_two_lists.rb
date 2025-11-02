f35bbc2b0140250aacf3be08d14ae9032373a5c4
Oleksandr Kozlov <wpok13@gmail.com>
2025-11-02 21:00:35 +0100

    Change method name

--- exercises_RB101-RB119/easy_7/combine_two_lists.rb
+++ exercises_RB101-RB119/easy_7/combine_two_lists.rb
@@ -1,4 +1,4 @@
-def combine_two_arrays(array1, array2)
+def interleave(array1, array2)
   new_array = []
   array1.each_with_index do |item, index|
     new_array << item
@@ -7,9 +7,9 @@ def combine_two_arrays(array1, array2)
   new_array
 end
 
-p combine_two_arrays([1, 2, 3], ['a', 'b', 'c'])
+p interleave([1, 2, 3], ['a', 'b', 'c'])
 # == Output ==
 # [1, "a", 2, "b", 3, "c"]
-p combine_two_arrays(['apple', 'banana', 'cherry'], [true, false, true])
+p interleave(['apple', 'banana', 'cherry'], [true, false, true])
 # == Output ==
 # ["apple", true, "banana", false, "cherry", true]
