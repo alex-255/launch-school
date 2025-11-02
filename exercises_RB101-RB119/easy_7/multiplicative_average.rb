2c8ed0144357687dd79b1b4e04febbce829638a4
Oleksandr Kozlov <wpok13@gmail.com>
2025-11-02 22:24:29 +0100

    Some changes

--- exercises_RB101-RB119/easy_7/multiplicative_average.rb
+++ exercises_RB101-RB119/easy_7/multiplicative_average.rb
@@ -1,13 +1,9 @@
-def multiplicative_average(array)
+def show_multiplicative_average(array)
   product = array.inject(1) { |prod, num| prod * num }
   average = product.to_f / array.size
-  format('%.3f', average)
+  puts "The result is " + format('%.3f', average)
 end
 
-p multiplicative_average([3, 5])
-# == Output ==
-# "7.500"
-p multiplicative_average([2, 5, 7, 11, 13, 17])
-# == Output ==
-# "28361.667"
-p multiplicative_average([1, 3, 5, 7, 9, 11, 13, 15, 17, 19])
+show_multiplicative_average([3, 5])
+show_multiplicative_average([2, 5, 7, 11, 13, 17])
+show_multiplicative_average([1, 3, 5, 7, 9, 11, 13, 15, 17, 19])
