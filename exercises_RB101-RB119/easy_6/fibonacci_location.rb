72d60f082b96a7cf4d6543b7a4a4e6e1cb4f6955
Oleksandr Kozlov <wpok13@gmail.com>
2025-11-02 16:13:05 +0100

    ready to feedback

--- exercises_RB101-RB119/easy_6/fibonacci_location.rb
+++ exercises_RB101-RB119/easy_6/fibonacci_location.rb
@@ -2,5 +2,18 @@ def fibonacci(n)
   first = 1
   last = 1 
   index = 2
-  
+  while last.to_s.length < n
+    fibonacci = first + last
+    first = last
+    last = fibonacci
+    index += 1
+  end
+  index
 end
+
+puts fibonacci(2)
+puts fibonacci(3)
+puts fibonacci(10)
+puts fibonacci(100)
+puts fibonacci(1000)
+puts fibonacci(10000)
