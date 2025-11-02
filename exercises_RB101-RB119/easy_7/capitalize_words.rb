f06875a15fbe0d93a368e9e520a061679bf13264
Oleksandr Kozlov <wpok13@gmail.com>
2025-11-02 21:16:17 +0100

    add method for capitalizing each word in string

---
+++ exercises_RB101-RB119/easy_7/capitalize_words.rb
@@ -0,0 +1,17 @@
+def capitalize_words(string)
+  words = string.split(/\s+/)
+  capitalized_words = words.map do |word|
+    word[0].upcase + word[1..-1].downcase
+  end
+  capitalized_words.join(' ')
+end
+
+p capitalize_words('four score and seven')
+# == Output ==
+# "Four Score And Seven"
+p capitalize_words('the javaScript language')
+# == Output ==
+# "The Javascript Language"
+puts capitalize_words('this is a "quoted" word')
+# == Output ==
+# 'This Is A "quoted" Word'
