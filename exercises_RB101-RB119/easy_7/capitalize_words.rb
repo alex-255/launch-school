fc17381794ee5182dececbbd0ccb8aa8c7b98cee
Oleksandr Kozlov <wpok13@gmail.com>
2025-11-02 21:19:37 +0100

    working now with differrent amount of spaces.

--- exercises_RB101-RB119/easy_7/capitalize_words.rb
+++ exercises_RB101-RB119/easy_7/capitalize_words.rb
@@ -1,12 +1,12 @@
 def capitalize_words(string)
-  words = string.split(/\s+/)
+  words = string.split(/(\s+)/)
   capitalized_words = words.map do |word|
     word[0].upcase + word[1..-1].downcase
   end
-  capitalized_words.join(' ')
+  capitalized_words.join('')
 end
 
-p capitalize_words('four score and seven')
+p capitalize_words('four  score and seven')
 # == Output ==
 # "Four Score And Seven"
 p capitalize_words('the javaScript language')
