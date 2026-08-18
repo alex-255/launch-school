a4df157a40e0380d38557b65655dda618a6b398f
Oleksandr Kozlov <wpok13@gmail.com>
2026-08-18 14:43:05 +0200

    Add to excercise 3

--- ruby-4/0_the_basics/3.rb
+++ ruby-4/0_the_basics/3.rb
@@ -2,3 +2,17 @@ movies = {:'Batman'=>2003, :'The Matrix'=> 1998}
 
 puts movies[:'Batman']
 puts movies[:'The Matrix']
+
+# and new syntax for creating hashes
+
+movies = { "jaws": 1975,
+           anchorman: 2004,
+           man_of_steel: 2013,
+           a_beautiful_mind: 2001,
+           the_evil_dead: 1981 }
+
+puts movies[:"jaws"] # it works
+puts movies[:anchorman]
+puts movies[:man_of_steel]
+puts movies[:a_beautiful_mind]
+puts movies[:the_evil_dead]
