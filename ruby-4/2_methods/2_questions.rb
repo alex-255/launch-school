21a574b21c2c838eb0a2d96b3f71da611e100ba6
Oleksandr Kozlov <wpok13@gmail.com>
2026-08-19 09:44:02 +0200

    exercise 2 - questions

---
+++ ruby-4/2_methods/2_questions.rb
@@ -0,0 +1,14 @@
+# 1. x = 2
+# 2
+
+# 2. puts x = 2
+# nil
+
+# 3. p name = "Joe"
+# "Joe" (p outputs and returns the value)
+
+# 4. four = "four"
+# "four"
+
+# 5. print something = "nothing"
+# nil (but printed in the same line =>     "nothing"=>nil)
