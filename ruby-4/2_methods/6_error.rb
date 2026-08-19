410594c5a6921111a943f7f5925aa0444b6a77f2
Oleksandr Kozlov <wpok13@gmail.com>
2026-08-19 09:54:41 +0200

    exercise 6 - error message

---
+++ ruby-4/2_methods/6_error.rb
@@ -0,0 +1,6 @@
+# ArgumentError: wrong number of arguments (1 for 2)
+#   from (irb):1:in `calculate_product'
+#   from (irb):4
+#   from /Users/username/.rvm/rubies/ruby-2.5.3/bin/irb:12:in `<main>'
+
+#   It has to be 2 arguments, while here we have only one.
